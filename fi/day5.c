aded75cfed7c2669a5d47160f40ce26085fc89e0
One234Fi <54912519+One234Fi@users.noreply.github.com>
2024-12-10 18:50:18 -0600

    day5 part 2

--- fi/day5.c
+++ fi/day5.c
@@ -3,6 +3,8 @@
 #include "file.h"
 #include "trie.h"
 #include "vector.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -90,5 +92,72 @@ void day5_part1(char * filepath) {
     fprintf(stdout, "%ld\n", sum);
 }
 
+
+
+long_map * ltr = 0;
+
+int compare(const void * a, const void * b) {
+    long al = *(long *) a; 
+    long bl = *(long *) b; 
+
+    long_set ** s = map_upsert(&ltr, al, NULL);
+    if (!!s) {
+        long * res = set_upsert(s, bl, NULL);
+        if (!!res) {
+            return -1;
+        }
+    }
+
+    s = map_upsert(&ltr, bl, NULL);
+    if (!!s) {
+        long * res = set_upsert(s, al, NULL);
+        if (!!res) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+bool is_ordered(vec_longs manual) {
+    for (ptrdiff_t i = 0; i < manual.len; i++) {
+        for (ptrdiff_t j = i; j < manual.len; j++) {
+            if (i == j) {
+                continue;
+            }
+            if (compare(&manual.data[i], &manual.data[j]) == 1) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 void day5_part2(char * filepath) {
+    string dat = file_parse(filepath);
+    arena a = arena_init(dat.len * 32);
+    vec_string lines = string_split(&a, dat, make_string("\n"));
+
+    ptrdiff_t i = 0;
+    string line = lines.data[i];
+    while (line.data[0] != '\n') { //go till empty line
+        vec_longs rule = parse_longs(&a, lines.data[i]);
+        long_set ** set = map_upsert(&ltr, rule.data[0], &a);
+        set_upsert(set, rule.data[1], &a);
+        i++;
+        line = lines.data[i];
+    }
+    i++; //skip empty line
+
+    long sum = 0;
+    for (; i < lines.len; i++) {
+        arena scratch = a;
+        vec_longs manual = parse_longs(&scratch, lines.data[i]);
+        if (!is_ordered(manual)) {
+            qsort(manual.data, manual.len, sizeof(manual.data[i]), compare);
+            sum += manual.data[manual.len / 2];
+        }
+    }
+
+    fprintf(stdout, "%ld\n", sum);
 } 
