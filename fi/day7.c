f7e4fa1747ea6830a6c8397a4eca16deb6f06184
One234Fi <54912519+One234Fi@users.noreply.github.com>
2024-12-12 21:16:13 -0600

    day 7 part 2

--- fi/day7.c
+++ fi/day7.c
@@ -35,4 +35,47 @@ void day7_part1(char * filepath) {
 }
 
 void day7_part2(char * filepath) {
+    arena a = arena_init(1024 * 1024);
+    string dat = file_parse(filepath);
+    vec_string lines = string_split(&a, dat, make_string("\n"));
+    long sum = 0;
+    for (long i = 0; i < lines.len; i++) {
+        arena t = a;
+        vec_longs line = parse_longs(&t, lines.data[i]);
+        long test = line.data[0];
+        long oplen = line.len - 2;
+        long config_count = powl(3, oplen);
+        for (long config = 0; config < config_count; config++) {
+            long res = line.data[1];
+            long bits = config;
+            for (long j = 0; j < oplen; j++) {
+                switch (bits % 3) {
+                    case 0:
+                        res += line.data[j+2];
+                        break;
+                    case 1:
+                        res *= line.data[j+2];
+                        break;
+                    case 2:
+                        {
+                            long p = 10;
+                            long l = line.data[j+2];
+                            while (l >= p) {
+                                p *= 10;
+                            }
+                            res *= p;
+                            res += l;
+                        }
+                        break;
+                }
+                bits /= 3;
+            }
+            if (res == test) {
+                sum += test;
+                break;
+            }
+        }
+    }
+    fprintf(stdout, "%ld\n", sum);
+
 }
