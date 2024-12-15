24a083802031accafba045f4fe51eacc2d8f09e4
One234Fi <54912519+One234Fi@users.noreply.github.com>
2024-12-14 23:29:25 -0600

    day8 part 2

--- fi/day8.c
+++ fi/day8.c
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     int x;
@@ -12,10 +13,15 @@ typedef struct {
     char f;
 } node;
 
+typedef struct head head;
+struct head {
+    head * next;
+    node * val;
+};
+
 bool node_equal(node a, node b) {
     return a.x == b.x 
-        && a.y == b.y 
-        && a.f == b.f;
+        && a.y == b.y ;
 }
 
 typedef struct node_set node_set;
@@ -24,7 +30,7 @@ struct node_set {
     node val;
 };
 
-node * node_upsert(node_set ** s, node val, arena * a) {
+node * node_upsert(node_set ** s, node val, head ** list, arena * a) {
     for (unsigned long h = hash(val); *s; h <<=2) {
         if (node_equal(val, (*s)->val)) {
             return &(*s)->val;
@@ -36,20 +42,24 @@ node * node_upsert(node_set ** s, node val, arena * a) {
     }
     *s = new(a, node_set);
     (*s)->val = val;
+    if (!!list) {
+        head * l = new(a, head);
+        l->val = &(*s)->val;
+        l->next = *list;
+        *list = l;
+    }
     return &(*s)->val;
 }
 
-
-long sum_node_set(node_set * s, string st, int w) {
+long sum_node_set(node_set * s) {
     if (!s) {
         return 0;
     }
-    st.data[w * s->val.y + s->val.x] = '#';
     return 1 + 
-        sum_node_set(s->child[0], st, w) +
-        sum_node_set(s->child[1], st, w) +
-        sum_node_set(s->child[2], st, w) +
-        sum_node_set(s->child[3], st, w);
+        sum_node_set(s->child[0]) +
+        sum_node_set(s->child[1]) +
+        sum_node_set(s->child[2]) +
+        sum_node_set(s->child[3]);
 }
 
 typedef struct {
@@ -103,7 +113,7 @@ void day8_part1(char * filepath) {
                     n.x < x && 
                     n.y >= 0 &&
                     n.y < y) {
-                    *node_upsert(&antinodes, n, &perm) = n;
+                    *node_upsert(&antinodes, n, NULL, &perm) = n;
                 }
 
                 if (b.x < a.x) {
@@ -120,26 +130,114 @@ void day8_part1(char * filepath) {
                     n.x < x && 
                     n.y >= 0 &&
                     n.y < y) {
-                    *node_upsert(&antinodes, n, &perm) = n;
+                    *node_upsert(&antinodes, n, NULL, &perm) = n;
                 }
             }
         }
     }
 
-    long sum = sum_node_set(antinodes, dat, x);
+    long sum = sum_node_set(antinodes);
     fprintf(stdout, "%s\n", dat.data);
     fprintf(stdout, "%ld\n", sum);
 }
 
-//
-// 5, 2
-// 7, 3
-//
-// 2-3 
-//
-//
-// x = 2d - d
+node make_antinode(node a, node b) {
+    node n = {0};
+    n.x = 2 * b.x - a.x;
+    n.y = 2 * b.y - a.y;
+    return n;
+}
 
+#define get(g, x_p, y_p) g.data[y_p * g.x + x_p]
+#define in_bounds(g, x_p, y_p) (x_p >= 0 && y_p >= 0 && x_p < g.x && y_p < g.y)
+typedef struct {
+    char * data;
+    int x;
+    int y;
+} grid;
+
+grid new_grid(arena * a, int x, int y) {
+    grid g = {0};
+    g.data = new(a, char, x * y);
+    memset(g.data, '.', x * y);
+    g.x = x;
+    g.y = y;
+    return g;
+}
+
+void print_grid(grid g, arena scratch) {
+    char * buf = new(&scratch, char, g.x + 1);
+    for (int i = 0; i < g.y; i++) {
+        memcpy(buf, g.data + (i * g.x), g.x);
+        buf[g.x] = '\0';
+        fprintf(stdout, "%s\n", buf);
+    }
+}
+
+void generate_antinodes(node a, node b, arena * perm, node_set **s, grid g) {
+    if (in_bounds(g, a.x, a.y)) {
+        get(g, a.x, a.y) = '#';
+        fprintf(stdout, "%d, %d\n", a.x, a.y);
+    }
+
+    node n = make_antinode(a, b);
+    if (in_bounds(g, n.x, n.y)) {
+        get(g, n.x, n.y) = '#';
+    }
+
+    node at = b;
+    node bt = n;
+    while(in_bounds(g, n.x, n.y)) {
+        n = make_antinode(at, bt);
+        if (in_bounds(g, n.x, n.y)) {
+            get(g, n.x, n.y) = '#';
+        }
+        at = bt;
+        bt = n;
+    }
+}
 
 void day8_part2(char * filepath) {
+    string dat = file_parse(filepath);
+    arena perm = arena_init(dat.len * 1024 * 2);
+
+    vec_string lines = string_split(&perm, dat, make_string("\n"));
+    node_set * antennas = 0;
+    head * list = new(&perm, head);
+
+    grid g = new_grid(&perm, lines.data[0].len, lines.len);
+    for (long y = 0; y < lines.len; y++) {
+        for (long x = 0; x < lines.data[y].len; x++) {
+            if (lines.data[y].data[x] != '.' && 
+                lines.data[y].data[x] != '\n') {
+                node n = {0};
+                n.x = x;
+                n.y = y;
+                n.f = lines.data[y].data[x];
+                *node_upsert(&antennas, n, &list, &perm) = n;
+            }
+        }
+    }
+
+    node_set * antinodes = 0;
+
+    for (head * i = list; i->next != NULL; i = i->next) {
+        for (head * j = list; j->next != NULL; j = j->next) {
+            node a = *i->val;
+            node b = *j->val;
+            if (a.f == b.f && i != j) {
+                generate_antinodes(b, a, &perm, &antinodes, g);
+                generate_antinodes(a, b, &perm, &antinodes, g);
+            }
+        }
+    }
+
+    long sum = 0;
+    for (long i = 0; i < g.x * g.y; i++) {
+        if (g.data[i] != '.') {
+            sum++;
+        }
+    }
+    print_grid(g, perm);
+    fprintf(stdout, "%ld\n", sum);
 }
